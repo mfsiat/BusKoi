64212f66cbf8288c2de83660b46b9889a342e61f
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-08-01 09:56:12 +0600

    Login Screen

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -13,7 +13,29 @@ class LoginScreen extends Component{
     render(){
         return (
             <View style={{ flex: 1 }}>
-                <ImageBackground></ImageBackground>
+                <ImageBackground 
+                    source={require('../assets/loginScreen.png')}
+                    style={{ flex: 1 }}
+                >
+                    <View style={{
+                        flex:1,
+                        justifyContent:'center',
+                        alignItems:'center'
+                    }}>
+                        <View style={{
+                            backgroundColor: 'Yellow',
+                            height: 100, 
+                            width: 100, 
+                            alignItems: 'center',
+                            justifyContent: 'center'
+                    }}>
+                        <Text style={{fontWeight: 'bold', fontSize: 26}}>
+                            BusKoi
+                        </Text>
+
+                        </View>
+                    </View>
+                </ImageBackground>
             </View>
         );
     }
