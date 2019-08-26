59c87525a57c148715d2653b8ff34577e68afb79
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-08-26 13:44:40 +0600

    styles searchBox

---
+++ Screens/Components/SearchBox/SearchBoxStyles.js
@@ -0,0 +1,10 @@
+import { Dimensions } from "react-native";
+var width = Dimensions.get("window").width; // full width
+
+const styles = {
+    searchBox:{
+        top: 0,
+        position: "absolute",
+        width: width
+    },
+}
