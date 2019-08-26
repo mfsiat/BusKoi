26015a581f8cffa0c2cc112d21249140ddfa7f6e
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-08-26 13:47:54 +0600

    styles second inputWrapper

--- Screens/Components/SearchBox/SearchBoxStyles.js
+++ Screens/Components/SearchBox/SearchBoxStyles.js
@@ -15,5 +15,14 @@ const styles = {
         backgroundColor:"#fff",
         opacity: 0.9,
         borderRadius: 7
+    },
+    secondInputWrapper:{
+        marginLeft: 15,
+        marginRight: 10,
+        marginTop: 10,
+        marginBottom: 0,
+        backgroundColor:"#fff",
+        opacity: 0.9,
+        borderRadius: 7
     }
 }
