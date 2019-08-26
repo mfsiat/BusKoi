b2e85fa5164592d5398b0ac43b0759fcaf603305
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-08-26 13:38:23 +0600

    adding the search box which is a stateless component

---
+++ Screens/Components/SearchBox/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View, Text} from "react-native";
+
+export const SearchBox = () =>{
+    return(
+        <View>
+
+        </View>
+    );
+}
+
+export default SearchBox;
