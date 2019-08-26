eea64070e3e85a391f1436e51238810cac4ffe18
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-08-27 00:34:18 +0600

    document formatted

--- Screens/SingupScreen.js
+++ Screens/SingupScreen.js
@@ -1,38 +1,44 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View, Text } from "react-native";
 
-import { Container, Content, Header, Form, Input, Item, Button, Label } from 'native-base';
+import {
+  Container,
+  Content,
+  Header,
+  Form,
+  Input,
+  Item,
+  Button,
+  Label
+} from "native-base";
 
 export default class SingupScreen extends Component {
   render() {
     return (
-        <Container style={styles.container}>
-            <Form>
-                <Item floatingLabel>
-                <Label>Email</Label>
-                    <Input 
-                        autoCorrect={false}
-                        autoCapitalize="none"
-                    />
-                </Item>
-                <Item floatingLabel>
-                <Label>Password</Label>
-                    <Input 
-                        secureTextEntry={true}
-                        autoCorrect={false}
-                        autoCapitalize="none"
-                    />
-                </Item>
-            </Form>
-        </Container>
+      <Container style={styles.container}>
+        <Form>
+          <Item floatingLabel>
+            <Label>Email</Label>
+            <Input autoCorrect={false} autoCapitalize="none" />
+          </Item>
+          <Item floatingLabel>
+            <Label>Password</Label>
+            <Input
+              secureTextEntry={true}
+              autoCorrect={false}
+              autoCapitalize="none"
+            />
+          </Item>
+        </Form>
+      </Container>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: '#fff',
-        justifyContent: 'center'
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center"
+  }
 });
