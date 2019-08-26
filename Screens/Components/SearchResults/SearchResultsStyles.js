999b9528c764d9ebf7996b51f00ee841a4bfb7f9
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-08-26 17:36:14 +0600

    added styles

---
+++ Screens/Components/SearchResults/SearchResultsStyles.js
@@ -0,0 +1,15 @@
+import { Dimensions } from "react-native";
+var width = Dimensions.get("window").width;
+const styles = {
+    searchResultsWrapper:{
+        top: 158,
+        position: "absolute",
+        width: width,
+        height: 100,
+        backgroundColor: "#fff",
+        opacity: 0.9
+    },
+    primaryText:{
+        
+    }
+}
