9b2906997ace3180464e9641d6c5685f0123cd7c
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-08-27 00:57:48 +0600

    update

--- Screens/SingupScreen.js
+++ Screens/SingupScreen.js
@@ -13,13 +13,35 @@ import {
 } from "native-base";
 
 export default class SingupScreen extends Component {
+
+  constructor(props) {
+          super(props)
+
+          this.state = ({
+            email: '',
+            password: ''
+          })
+  }
+
+  singUpUser = (email,password) => {
+
+  }
+
+  loginUser = (email, password) => {
+
+  }
+
+
   render() {
     return (
       <Container style={styles.container}>
         <Form>
           <Item floatingLabel>
             <Label>Email</Label>
-            <Input autoCorrect={false} autoCapitalize="none" />
+            <Input 
+                autoCorrect={false} 
+                autoCapitalize="none" 
+                onChangeText={(email) => this.setState({email})}/>
           </Item>
           <Item floatingLabel>
             <Label>Password</Label>
@@ -29,8 +51,21 @@ export default class SingupScreen extends Component {
               autoCapitalize="none"
             />
           </Item>
-          <Button style={{ marginTop: 10 }} full rounded success>
-            <text>Login</text>
+          <Button style={{ marginTop: 10 }} 
+            full 
+            rounded 
+            success
+            onPress = {()=> this.loginUser(this.state.email, this.state.password)}          
+          >
+            <Text style={{ color: 'white' }} >Login</Text>
+          </Button>
+          <Button style={{ marginTop: 10 }} 
+            full 
+            rounded 
+            primary
+            onPress = {()=> this.singUpUser(this.state.email, this.state.password)}
+        >
+            <Text style={{ color: 'white' }} >Sing-Up</Text>
           </Button>
         </Form>
       </Container>
