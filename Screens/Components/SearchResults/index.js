ce9fc4ae08e20b664aea702129ac16d7090a407d
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-08-26 17:33:38 +0600

    modified

---
+++ Screens/Components/SearchResults/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Text} from "react-native";
+import { View, InputGroup, Input } from "native-base";
+
+import Icon from "react-native-vector-icons/FontAwesome";
+
+import styles from "./SearchResultsStyles.js";
+
+export const SearchResults = () =>{
+    return(
+        <View>
+
+        </View>
+    );
+}
+
+export default SearchResults;
