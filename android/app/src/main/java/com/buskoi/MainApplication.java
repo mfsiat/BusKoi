58c9684d52cb5f5885f0345ed4c7a939c33df2d3
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-08-26 13:09:43 +0600

    modified to match the requirements

--- android/app/src/main/java/com/buskoi/MainApplication.java
+++ android/app/src/main/java/com/buskoi/MainApplication.java
@@ -11,6 +11,7 @@ import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
 
 import java.util.List;
 
@@ -27,7 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
       // Packages that cannot be autolinked yet can be added manually here, for example:
-      // packages.add(new MyReactNativePackage());
+      // packages.add(new RNGooglePlacesPackage());
       return packages;
     }
 
