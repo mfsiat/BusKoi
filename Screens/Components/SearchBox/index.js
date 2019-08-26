a9269de0c470a13678c41cc667c68382e88426ab
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-08-26 14:39:25 +0600

    added icon on the search box

--- Screens/Components/SearchBox/index.js
+++ Screens/Components/SearchBox/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import {Text} from "react-native";
 import { View, InputGroup, Input } from "native-base";
 
+import Icon from "react-native-vector-icons/FontAwesome";
+
 import styles from "./SearchBoxStyles.js";
 
 export const SearchBox = () =>{
@@ -9,6 +11,7 @@ export const SearchBox = () =>{
         <View style={styles.searchBox}>
             <Text style={styles.label}>Where To?</Text>
             <InputGroup>
+                <Icon name="search" size={15} color="#FF5E3A" />
                 <Input style={styles.inputSearch} placeholder="Pick Up Location"></Input>
             </InputGroup>
         </View>
